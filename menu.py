3951044ab960837df473cb9c2bce3321999da938
Anna Balan <colnyshkasmile@gmail.com>
2018-01-09 20:59:44 +0000

    spacing edits

--- menu.py
+++ menu.py
@@ -1,24 +1,24 @@
-import os 
-import template
+import os
+import recipe-template.py
 
 print("\n\n\t\t\t'Welcome to Cookbook'\n")
-choice = None 
-while choice != "0": 
-  print( """ Menu
-        0 - Exit 
-        1 - Show Recipe Menu 
-        2 - Look Up Recipe 
+choice = None
+while choice != "0":
+    print(""" Menu
+        0 - Exit
+        1 - Show Recipe Menu
+        2 - Look Up Recipe
         3 - Add Recipe """)
 
-  choice = input("Choice: ") 
-  print() 
-  if choice == "0": 
-    print("Good-bye.") 
-  elif choice == "1": 
-    os.system('cls||clear') 
-    print(*****) 
-  elif choice == "2": 
-    os.system('cls||clear') 
-    x = input("Enter Recipe Name: ")
-  else: print("Invalid choice")
-   
+    choice = input("Choice: ")
+    print()
+    if choice == "0":
+        print("Good-bye.")
+    elif choice == "1":
+        os.system('cls||clear')
+        print()
+    elif choice == "2":
+        os.system('cls||clear')
+        x = input("Enter Recipe Name: ")
+    else:
+        print("Invalid choice")
